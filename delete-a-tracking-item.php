b2037ba8063a71eb1614d63dc12399b49b533ee3
clooney <phpjd@qq.com>
2017-05-02 17:01:26 +0800

    china post  tracking  api
    China Post tracking API and webhook make it easy to integrate China Post
    Tracking function into your own project. Support APIs Client Libraries
    for PHP

---
+++ delete-a-tracking-item.php
@@ -0,0 +1,7 @@
+<?php
+
+include("track.class.php");
+$track = new Trackingmore;
+$track = $track->deleteTrackingItem('china-post','RM131516216CN');
+
+?>
---
+++ get-tracking-results-of-a-single-tracking.php
@@ -0,0 +1,7 @@
+<?php
+
+include("track.class.php");
+$track = new Trackingmore;
+
+$track = $track->getSingleTrackingResult('china-post','LK166607403CN');
+?>
